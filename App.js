050e1e2f72889abcb4b025f8a4c29ec59012fd30
Felipe Gonzalez <contraviento@gmail.com>
2024-04-24 10:45:36 -0700

    add styles for each task that will be created

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.taskWrapper}>
+				<Text style={styles.sectionTitle}>Today's task</Text>
+
+				<View style={styles.items}>
+					<Task text={'task 1'} />
+					<Task text={'task 2'} />
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#e8eaed',
+	},
+	taskWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	items: {
+		marginTop: 10,
+	},
 });
